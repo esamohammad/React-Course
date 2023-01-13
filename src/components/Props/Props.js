1c06ec1b32658e4f7e30ea4f39ed95d15c04d043
mdesabd <esa.ce2020@gmail.com>
2023-01-13 23:31:11 +0600

    destruccturing

--- src/components/Props/Props.js
+++ src/components/Props/Props.js
@@ -3,9 +3,10 @@ import React from 'react';
 
 
 const Props = (props) => {
+    const { titleText, description } = props;
     return (
         <div>
-            <h3>{props.titleText} and and call him {props.description}</h3>
+            <h3>{titleText} and and call him {description}</h3>
         </div>
     );
 };
