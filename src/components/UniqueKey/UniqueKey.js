eab9a4411b5c02d753cc8d15402b3741a8685e7c
mdesabd <esa.ce2020@gmail.com>
2023-01-14 04:18:20 +0600

    unique id in json

--- src/components/UniqueKey/UniqueKey.js
+++ src/components/UniqueKey/UniqueKey.js
@@ -3,22 +3,27 @@ import { v4 as uuidv4 } from 'uuid';
 
 const names = [
     {
+        "id": "uuidv4()",
         "title": "Esa",
         "desc": "description-1"
     },
     {
+        "id": "uuidv4()",
         "title": "Hamdu",
         "desc": "description-2"
     },
     {
+        "id": "uuidv4()",
         "title": "Redwan",
         "desc": "description-3"
     },
     {
+        "id": "uuidv4()",
         "title": "Moni",
         "desc": "description-4"
     },
     {
+        "id": "uuidv4()",
         "title": "Mona",
         "desc": "description-5"
     }
@@ -30,12 +35,12 @@ const UniqueKey = () => {
     return (
         <div>
             {names.map((name) =>
-                <div key={uuidv4()}>
+                <div key={name.id}>
                     <h3>{name.title}</h3>
                     <p>{name.desc}</p>
-                    
+
                 </div>)}
-             
+
         </div>
     );
 };
