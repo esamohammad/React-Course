5571077f3d060995e0daf17ca3c1ae7bac0fb564
mdesabd <esa.ce2020@gmail.com>
2023-01-14 21:49:45 +0600

    react icons- more and more imports

--- src/components/ReactIcons/ReactIcons.js
+++ src/components/ReactIcons/ReactIcons.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { FaBeer } from 'react-icons/fa';
+import { FaBeer   } from 'react-icons/fa';
+import { AiFillAlert ,AiFillCar,AiFillCrown,AiFillDingtalkSquare } from "react-icons/ai";
+import { FcDislike } from "react-icons/fc";
 
 const ReactIcons = () => {
     return (
         <div>
-            <h3> Lets go for a <FaBeer />? </h3>
+            <h1> Lets go for a <FaBeer />? </h1>
+            <h1> Lets go for a <AiFillAlert />? </h1>
+            <h1> Lets go for a <AiFillCar />? </h1>
+            <h1> Lets go for a <AiFillCrown />? </h1>
+            <h1> Lets go for a <AiFillDingtalkSquare />? </h1>
+            <h1> Don`t Love <FcDislike />? </h1>
         </div>
     );
 };
@@ -12,3 +19,5 @@ const ReactIcons = () => {
 export default ReactIcons;
 
 //icon use kora r joono ... npm packege nilam ar por documentation ache ... sei onujayi kaj korlei hobe'
+// https://react-icons.github.io/react-icons/icons?name=fc
+// prottek catagorir jonno akta link import korte hobe;
