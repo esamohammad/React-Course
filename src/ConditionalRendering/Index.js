a6788471c989a43b13bc6192290a32875324a1c5
mdesabd <esa.ce2020@gmail.com>
2023-01-15 15:45:20 +0600

    conditional rendaring system -2 element variable

--- src/ConditionalRendering/Index.js
+++ src/ConditionalRendering/Index.js
@@ -1,3 +1,4 @@
+/*
 import React, { Component } from 'react';
 import Login from './Login';
 import HomePage from './HomePage';
@@ -25,3 +26,41 @@ class Index extends Component {
 export default Index;
 
 //conditional rendaring system -1
+
+*/
+
+
+
+import React, { Component } from 'react';
+import Login from './Login';
+import HomePage from './HomePage';
+
+class Index extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isLoggedIn: false
+        }
+    }
+
+
+    render() {
+        const {isLoggedIn}=this.state //destructuring
+        let element;
+        
+        if (isLoggedIn) {
+            element= <HomePage></HomePage>
+        }
+        else {
+            element= <Login></Login>
+        }
+        return(
+            <div>{element}</div>
+        )
+    }
+}
+
+export default Index;
+
+//conditional rendaring system -2 element variable
