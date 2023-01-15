3c77f418b5f1b31b357df2c77bb96a35ba52f8ff
mdesabd <esa.ce2020@gmail.com>
2023-01-15 15:57:39 +0600

    conditional rendaring system -3 ,Ternary Method-1

--- src/ConditionalRendering/Index.js
+++ src/ConditionalRendering/Index.js
@@ -31,6 +31,19 @@ export default Index;
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 import React, { Component } from 'react';
 import Login from './Login';
 import HomePage from './HomePage';
@@ -64,3 +77,49 @@ class Index extends Component {
 export default Index;
 
 //conditional rendaring system -2 element variable
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+//conditional rendaring system -3 ,Ternary Method-1
+
+import React, { Component } from 'react';
+import Login from './Login';
+import HomePage from './HomePage';
+
+class Index extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isLoggedIn: false
+        }
+    }
+
+
+    render() {
+        const { isLoggedIn } = this.state //destructuring
+
+        let element;
+        element = isLoggedIn ? <HomePage /> : <Login />
+
+        return (
+            <div>
+                {element}
+            </div>
+        )
+    }
+}
+
+export default Index;
+
+//conditional rendaring system -3 ,Ternary Method-1
